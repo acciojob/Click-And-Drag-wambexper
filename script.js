3d7265aff8ec42c11cf2ba4407129a472b25f214
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 14:38:10 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your code here.
+  const items = document.querySelectorAll('.item');
+    let dragItem = null;
+
+    // Add dragstart event listener to each cube
+    items.forEach(item => {
+      item.addEventListener('dragstart', (e) => {
+        dragItem = e.target;
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', e.target.innerHTML);
+      });
+    });
+
+    // Add dragover event listener to the items container
+    document.querySelector('.items').addEventListener('dragover', (e) => {
+      e.preventDefault();
+    });
+
+    // Add drop event listener to the items container
+    document.querySelector('.items').addEventListener('drop', (e) => {
+      e.preventDefault();
+      if (dragItem) {
+        dragItem.innerHTML = e.dataTransfer.getData('text/html');
+        dragItem = null;
+      }
+    });
